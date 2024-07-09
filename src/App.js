b6e944c0b745789ab2e18df3c8fdcd1767d7a6c3
sari-rahul <sarisivadas93@gmail.com>
2024-07-09 12:18:12 +0200

    Add Root component and style it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client'; 
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider,} from "react-router-dom";
+
+import Root from './components/Root';
+import Dashboard from './components/Dashboard';
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/dashboard" element={<Dashboard />} />
+      {/* ... etc. */}
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <h2>Hello</h2>
-    </div>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
-export default App;
+export default App; 
